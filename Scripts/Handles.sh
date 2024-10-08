806a9179898617995a687ab875a826f200053488
lpssph <97702437+lpssph@users.noreply.github.com>
2024-10-09 07:41:36 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
+
+#修复freeswitch依赖缺失
+FW_FILE=$(find ../feeds/telephony/ -maxdepth 3 -type f -wholename "*/freeswitch/Makefile")
+if [ -f "$FW_FILE" ]; then
+	sed -i "s/libpcre/libpcre2/g" $FW_FILE
+
+	cd $PKG_PATCH && echo "freeswitch has been fixed!"
+fi
